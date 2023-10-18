ba3d5384e77617ff819a90ed4e1c38c7c255353e
tequilex <dron2010xxx@mail.ru>
2023-10-18 18:08:30 +0300

    add burger menu

--- src/components/navBar/navBar.component.jsx
+++ src/components/navBar/navBar.component.jsx
@@ -1,19 +1,57 @@
+import { useState, useEffect } from "react";
 import { ReactComponent as CactusLogo } from "../../assets/cactus.svg";
 
 import "./navBar.styles.scss";
 
 const NavBar = () => {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const [isActive, setIsActive] = useState("navbar__wrapper unactive");
+  const [burger__class, setBurger__class] = useState("burger__bar unclicked");
+
+  useEffect(() => {
+    if (isMenuClicked) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = 'visible';
+  }, [isMenuClicked]);
+
+  const toggler = () => {
+    if (!isMenuClicked) {
+      setIsActive("navbar__wrapper active");
+      setBurger__class("burger__bar clicked")
+    } else {
+      setIsActive("navbar__wrapper unactive");
+      setBurger__class("burger__bar unclicked")
+    }
+    setIsMenuClicked(!isMenuClicked)
+  };
+
+  const hideMenu = () => {
+    setIsMenuClicked(false)
+    setIsActive("navbar__wrapper unactive");
+    setBurger__class("burger__bar unclicked")
+  }
+
   return (
-    <div className="navbar__wrapper">
+    <div className={isActive}>
       <div className="navbar">
         <div className="logo">
-          <CactusLogo className="logo__img"/>
+          <CactusLogo className="logo__img" />
           <div className="logo__text">TEQUILEX</div>
+          <div className="burger__menu" onClick={toggler}>
+            <div className={burger__class}></div>
+            <div className={burger__class}></div>
+            <div className={burger__class}></div>
+          </div>
         </div>
         <div className="nav__links">
-          <a href="#about" className="nav__link">About</a>
-          <a href="#projects" className="nav__link">Projects</a>
-          <a href="#contacts" className="nav__link">Contact</a>
+          <a href="#about" className="nav__link" onClick={hideMenu}>
+            About
+          </a>
+          <a href="#projects" className="nav__link" onClick={hideMenu}>
+            Projects
+          </a>
+          <a href="#contacts" className="nav__link" onClick={hideMenu}>
+            Contact
+          </a>
         </div>
       </div>
     </div>
